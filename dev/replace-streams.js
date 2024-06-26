818b515174e499ac29375505d3ac81e4f2bbd6d7
paolo.liberti <paolo.liberti@eveilidia.com>
2024-06-26 22:14:27 +0200

    fix remove stream audio / video

--- dev/replace-streams.js
+++ dev/replace-streams.js
@@ -9,49 +9,77 @@ this.resetVideoStreams = function(streams, type) {
 function resetVideoStreams(streams, type) {
     streams = streams || arrayOfMediaStreams;
    
-    videos = [];
     arrayOfMediaStreams = [];
 
     var activeAudios = []
+    var activeVideos = []
 
     streams.forEach(function(stream) {
         arrayOfMediaStreams.push(stream); 
 
         if (stream.getTracks().filter(function(t) {
-                return t.kind === 'video';
-            }).length) {
+            return t.kind === 'video';
+        }).length && type === 'video') {
                 console.log("🚀 ~ streams video forEach ~ t:", stream);
                 var video = getVideo(stream);
                 video.stream = stream;
-                videos.push(video);
+                activeVideos.push(video);
         }
 
-        if (stream.getTracks().filter(function(t) {
+        else if (stream.getTracks().filter(function(t) {
             return t.kind === 'audio';
-        }).length) {
+        }).length && type === 'audio') {
             try {
                 console.log("🚀 ~ streams audio forEach ~ t:", stream);
+
                 var activeAudio = audios.filter(function(a){
                     console.log("🚀 ~ audios.filter ~ a:", a)
-                    return a["audioSource"].mediaStream === stream
+                    try {
+                        return a["audioSource"].mediaStream === stream   
+                    } catch (error) {
+                        console.error("error dentro filter ", error)
+                        return;
+                    }
                 })
-                if (activeAudio) activeAudios.push(activeAudio)                
+                if (activeAudio) activeAudios.push(activeAudio[0])                
             } catch (error) {
                 console.error("ERROR FATALE: ", error)
             }
 
         }        
     });
-    var removedAudios = audios.filter(function(audio) {
-        return !activeAudios.some(function(audioStream) {
-            return audio["audioSource"].mediaStream === audioStream;
-        });
-    });
+
+    if(type === 'video'){
+        videos = activeVideos;
+    }
     
     if(type === 'audio'){
-        audios = activeAudios;
-        console.log("🚀 ~ [END ] resetVideoStreams ~ audios:", audios)
+        try {
+            console.log("🚀 ~ removedAudios ~ audios:", audios)
+            console.log("🚀 ~ removedAudios ~ videos:", videos)
+            console.log("🚀 ~ return!activeAudios.some ~ activeAudios:", activeAudios)            
+            var removedAudios = audios.filter(function(audio) {
+                return !activeAudios.some(function(ac) {
+                    console.log(" **************** DENTRO SOME FILTER REMOVE AUDIOS")
+                    console.log("🚀 ~ return!activeAudios.some ~ audio[audioSource].mediaStream:", audio)
+                    console.log("🚀 ~ return!activeAudios.some ~ ac[audioSource].mediaStream:", ac)
+                    console.log("[END ] **************** DENTRO SOME FILTER REMOVE AUDIOS")
+                    return audio["audioSource"].mediaStream === ac["audioSource"].mediaStream;
+                });
+            });
+            console.log("🚀 ~ removedAudios ~ removedAudios:", removedAudios)
+    
+            audios = activeAudios;
+    
+            console.log("🚀 ~ [END ] resetVideoStreams ~ audios:", audios)            
+        } catch (error) {
+            console.error("ERRORE TYPE audio: ", error)
+        }
+
+
         removedAudios.forEach(function(audio) {
+            console.log("🚀 ~ removedAudios.forEach ~ audio:", audio)
+            console.log("🚀 ~ removedAudios.forEach ~ typeof audio:", typeof audio)
             console.log(typeof audio["gainNode"])
             console.log("🚀 ~ audios.forEach ~ gainNode:", audio["gainNode"])
             audio["gainNode"].gain.value = 0;
@@ -62,19 +90,4 @@ function resetVideoStreams(streams, type) {
             }
         })
     }
-
-    // console.log("🚀 ~ [END ] resetVideoStreams ~ audios:", audios)
-    // console.log("🚀 ~ [END ] resetVideoStreams ~ activeAudios:", activeAudios)
-    // console.log("🚀 ~ [END ] resetVideoStreams ~ removedAudios:", removedAudios)    
-    // removedAudios.forEach(function(mediaSourceAudioNode) {
-    //     mediaSourceAudioNode = mediaSourceAudioNode[0]
-    //     console.log("************************ removed audio");
-    //     console.log(mediaSourceAudioNode);
-    //     console.log(typeof mediaSourceAudioNode);
-    //     try {
-    //         mediaSourceAudioNode.disconnect();
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // });
 }
